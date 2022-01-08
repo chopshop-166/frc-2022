179a904062dc8c8eb17b6c5e7e795cc046f927a2
Matt Soucy <git@msoucy.me>
2022-01-08 15:01:36 -0500

    Chopshop Extensions

---
+++ src/main/java/frc/robot/maps/RobotMap.java
@@ -0,0 +1,9 @@
+package frc.robot.maps;
+
+import com.chopshop166.chopshoplib.maps.RobotMapFor;
+
+// Need to get MAC address for roborio
+@RobotMapFor("Default")
+public class RobotMap {
+
+}
--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -7,8 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
 
   @Override
   public void periodic() {
