33b69a86012ea567ba6fdef8356a6d618723f48e
Jeff Burke <jburke0519@gmail.com>
2022-01-26 19:51:03 -0500

    We have a mock swerve module
    Takes in a location

---
+++ src/main/java/com/chopshop166/chopshoplib/drive/MockSwerveModule.java
@@ -0,0 +1,45 @@
+package com.chopshop166.chopshoplib.drive;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.SendableBuilder;
+
+public class MockSwerveModule implements SwerveModule {
+
+    private Translation2d location = new Translation2d();
+    private SwerveModuleState desiredState = new SwerveModuleState();
+
+    /**
+     * @param location Location of the module
+     */
+    public MockSwerveModule(final Translation2d location) {
+        this.location = location;
+    }
+
+    /**
+     * Get the modules location in relation to the CoM of the robot.
+     *
+     * @return Location2d object representing the offset
+     */
+    @Override
+    public Translation2d getLocation() {
+        return location;
+    }
+
+    /**
+     * Process the desired state and set the output values for the motor
+     * controllers.
+     * 
+     * @param desiredState The direction and speed.
+     */
+    @Override
+    public void setDesiredState(final SwerveModuleState desiredState) {
+        this.desiredState = desiredState;
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        // TODO Auto-generated method stub
+
+    }
+}
