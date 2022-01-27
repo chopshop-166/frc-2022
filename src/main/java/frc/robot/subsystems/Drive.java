0035964d86deae976489eb87f9d9a366850b3635
Jeff Burke <jburke0519@gmail.com>
2022-01-26 20:14:18 -0500

    Added a Drive Constructor
    Sets all private variables

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,12 +4,52 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.chopshop166.chopshoplib.commands.SmartSubsystemBase;
+import com.chopshop166.chopshoplib.drive.SwerveModule;
 
-public class Drive extends SubsystemBase {
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import frc.robot.maps.RobotMap.DriveMap;
+
+public class Drive extends SmartSubsystemBase {
+
+  private final SwerveDriveKinematics kinematics;
+  private final SwerveModule frontLeft;
+  private final SwerveModule frontRight;
+  private final SwerveModule rearLeft;
+  private final SwerveModule rearRight;
+
+  private final double maxDriveSpeedMetersPerSecond;
+  private final double maxRotationRadiansPerSecond;
+  private final Gyro gyro;
+
+  public Drive(final DriveMap map) {
+    super();
+
+    frontLeft = map.getFrontLeft();
+    frontRight = map.getFrontRight();
+    rearLeft = map.getRearLeft();
+    rearRight = map.getRearRight();
+    kinematics = new SwerveDriveKinematics(frontLeft.getLocation(), frontRight.getLocation(),
+        rearLeft.getLocation(), rearRight.getLocation());
+    gyro = map.getGyro();
+    maxDriveSpeedMetersPerSecond = map.getMaxDriveSpeedMetersPerSecond();
+    maxRotationRadiansPerSecond = map.getMaxRotationRadianPerSecond();
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void reset() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void safeState() {
+    // TODO Stop all movement
+
   }
 }
