788b34ed4f12c26a0d2de0496e18ce8bd65b256b
Jeff Burke <jburke0519@gmail.com>
2022-01-26 19:58:08 -0500

    Added a default drive map

--- src/main/java/frc/robot/maps/RobotMap.java
+++ src/main/java/frc/robot/maps/RobotMap.java
@@ -1,9 +1,95 @@
 package frc.robot.maps;
 
+import com.chopshop166.chopshoplib.drive.MockSwerveModule;
+import com.chopshop166.chopshoplib.drive.SwerveModule;
 import com.chopshop166.chopshoplib.maps.RobotMapFor;
+import com.chopshop166.chopshoplib.motors.SmartMotorController;
+import com.chopshop166.chopshoplib.sensors.MockGyro;
+import com.ctre.phoenix.sensors.CANCoder;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 // Need to get MAC address for roborio
 @RobotMapFor("Default")
 public class RobotMap {
+    public static class DriveMap {
+        // All Distances are in Meters
 
+        private final SwerveModule frontLeft;
+        private final SwerveModule frontRight;
+        private final SwerveModule rearLeft;
+        private final SwerveModule rearRight;
+        private final double maxDriveSpeedMetersPerSecond;
+        private final double maxRotationRadianPerSecond;
+        private final Gyro gyro;
+
+        public DriveMap() {
+
+            this.frontLeft = new MockSwerveModule(new Translation2d(0.381, 0.381));
+
+            this.frontRight = new MockSwerveModule(new Translation2d(0.381, -0.381));
+
+            this.rearLeft = new MockSwerveModule(new Translation2d(-0.381, 0.381));
+
+            this.rearRight = new MockSwerveModule(new Translation2d(-0.381, -0.381));
+
+            this.maxDriveSpeedMetersPerSecond = 2;
+
+            this.maxRotationRadianPerSecond = Math.PI;
+
+            this.gyro = new MockGyro();
+        }
+
+        public DriveMap(final SwerveModule frontLeft, final SwerveModule frontRight, final SwerveModule rearLeft,
+                final SwerveModule rearRight, final double maxDriveSpeedMetersPerSecond,
+                final double maxRotationRadianPerSecond, final Gyro gyro) {
+
+            this.frontLeft = frontLeft;
+
+            this.frontRight = frontRight;
+
+            this.rearLeft = rearLeft;
+
+            this.rearRight = rearRight;
+
+            this.maxDriveSpeedMetersPerSecond = maxDriveSpeedMetersPerSecond;
+
+            this.maxRotationRadianPerSecond = maxRotationRadianPerSecond;
+
+            this.gyro = gyro;
+        }
+
+        public SwerveModule getFrontLeft() {
+            return frontLeft;
+        }
+
+        public SwerveModule getFrontRight() {
+            return frontRight;
+        }
+
+        public SwerveModule getRearLeft() {
+            return rearLeft;
+        }
+
+        public SwerveModule getRearRight() {
+            return rearRight;
+        }
+
+        public double getMaxDriveSpeedMetersPerSecond() {
+            return maxDriveSpeedMetersPerSecond;
+        }
+
+        public double getMaxRotationRadianPerSecond() {
+            return maxRotationRadianPerSecond;
+        }
+
+        public Gyro getGyro() {
+            return gyro;
+        }
+    }
+
+    public DriveMap getDriveMap() {
+        return new DriveMap();
+    }
 }
