252d1ac86392c185cb4a9a23bc66175554ef9c48
Jeff Burke <jburke0519@gmail.com>
2022-01-26 19:14:05 -0500

    Discarded Unnecessary Function

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -12,9 +12,4 @@ public class Drive extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
   }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
 }
