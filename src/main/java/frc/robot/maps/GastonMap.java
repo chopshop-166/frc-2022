4035be9c6944b5b937c8c706afb27e69be10c99f
Noah M <namotyl@yahoo.com>
2022-01-30 12:55:33 -0500

    added some of the pid stuff for the intake

--- src/main/java/frc/robot/maps/GastonMap.java
+++ src/main/java/frc/robot/maps/GastonMap.java
@@ -8,6 +8,7 @@ import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 @RobotMapFor("Gaston")
 public class GastonMap extends RobotMap {
@@ -17,12 +18,41 @@ public class GastonMap extends RobotMap {
     // makes motor controller object
     private PIDSparkMax deploymentMotor = new PIDSparkMax(2, MotorType.kBrushless);
 
+    // initializes relative encoder and pid controller
     private RelativeEncoder deploymentEncoder = deploymentMotor.getEncoder().getRaw();
     private final SparkMaxPIDController deploymentPidController = deploymentMotor.getPidController();
 
-    // PID coefficients
-
     public IntakeMap getIntakeMap() {
+        // PID coefficients
+
+        // feel free to change these values as needed
+        // they are example values
+
+        deploymentPidController.setP(5e-5);
+        deploymentPidController.setI(1e-6);
+        deploymentPidController.setD(0);
+        deploymentPidController.setIZone(0);
+        deploymentPidController.setFF(0.000156);
+        deploymentPidController.setOutputRange(-1, 1);
+
+        // Smart motion coefficients
+        // Max velocity is in rpm
+        deploymentPidController.setSmartMotionMaxVelocity(2000, 0);
+        deploymentPidController.setSmartMotionMinOutputVelocity(-1, 0);
+        deploymentPidController.setSmartMotionMaxAccel(1500, 0);
+        // deploymentPidController.setSmartMotionAllowedClosedLoopError(allowedErr,
+        // slotID)
+
+        // display PID coefficients on SmartDashboard
+        /*
+         * SmartDashboard.putNumber("P Gain", kP);
+         * SmartDashboard.putNumber("I Gain", kI);
+         * SmartDashboard.putNumber("D Gain", kD);
+         * SmartDashboard.putNumber("I Zone", kIz);
+         * SmartDashboard.putNumber("Feed Forward", kFF);
+         * SmartDashboard.putNumber("Max Output", kMaxOutput);
+         * SmartDashboard.putNumber("Min Output", kMinOutput);
+         */
 
         IntakeMap map = new IntakeMap(new PIDSparkMax(3, MotorType.kBrushless),
                 deploymentMotor, outsideLimit::get, insideLimit::get);
