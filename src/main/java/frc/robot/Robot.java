122408e500ea4b0020b27ef7c4728d0528e3cc7c
Noah M <namotyl@yahoo.com>
2022-02-02 09:49:25 -0500

    button controller added

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,27 @@
 package frc.robot;
 
 import com.chopshop166.chopshoplib.commands.CommandRobot;
+import com.chopshop166.chopshoplib.controls.ButtonXboxController;
+
+import frc.robot.maps.RobotMap;
+import frc.robot.subsystems.Intake;
 
 public class Robot extends CommandRobot {
 
+  private final ButtonXboxController controller = new ButtonXboxController(0);
+
+  private final RobotMap map = getMapForName("Gaston", RobotMap.class, "frc.robot.maps");
+
+  private final Intake intake = new Intake(map.getIntakeMap());
+
   @Override
   public void robotInit() {
-    // Nothing to initialize
+    super.robotInit();
   }
 
   @Override
   public void configureButtonBindings() {
-
+    controller.getAButton();
   }
 
   @Override
