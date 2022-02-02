8262da746a637babf7bac1672f10e96f0be1cd1d
Noah M <namotyl@yahoo.com>
2022-02-01 20:12:01 -0500

    Updates pid for intake
    it probably might work now :)

--- src/main/java/frc/robot/maps/GastonMap.java
+++ src/main/java/frc/robot/maps/GastonMap.java
@@ -2,57 +2,57 @@ package frc.robot.maps;
 
 import com.chopshop166.chopshoplib.maps.RobotMapFor;
 import com.chopshop166.chopshoplib.motors.PIDSparkMax;
-import com.revrobotics.REVLibError;
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 @RobotMapFor("Gaston")
 public class GastonMap extends RobotMap {
     private final DigitalInput outsideLimit = new DigitalInput(4);
     private final DigitalInput insideLimit = new DigitalInput(5);
 
-    // makes motor controller object
+    // initializes relative encoder and pid controller, we don't need the encoder rn
+
     private PIDSparkMax deploymentMotor = new PIDSparkMax(2, MotorType.kBrushless);
 
-    // initializes relative encoder and pid controller
-    private RelativeEncoder deploymentEncoder = deploymentMotor.getEncoder().getRaw();
+    // private RelativeEncoder deploymentEncoder =
+    // deploymentMotor.getEncoder().getRaw();
     private final SparkMaxPIDController deploymentPidController = deploymentMotor.getPidController();
 
     public IntakeMap getIntakeMap() {
         // PID coefficients
 
-        // feel free to change these values as needed
-        // they are example values
-
-        deploymentPidController.setP(5e-5);
-        deploymentPidController.setI(1e-6);
-        deploymentPidController.setD(0);
-        deploymentPidController.setIZone(0);
-        deploymentPidController.setFF(0.000156);
-        deploymentPidController.setOutputRange(-1, 1);
+        double P = 5e-5;
+        double I = 1e-6;
+        double D = 0;
+        double IZone = 0;
+        double feedForward = 0.000156;
+        double maxOutput = 1;
+        double minOutput = -1;
 
         // Smart motion coefficients
-        // Max velocity is in rpm
-        deploymentPidController.setSmartMotionMaxVelocity(2000, 0);
-        deploymentPidController.setSmartMotionMinOutputVelocity(-1, 0);
-        deploymentPidController.setSmartMotionMaxAccel(1500, 0);
-        // deploymentPidController.setSmartMotionAllowedClosedLoopError(allowedErr,
-        // slotID)
+        // max velocity is in rpm!!
 
-        // display PID coefficients on SmartDashboard
-        /*
-         * SmartDashboard.putNumber("P Gain", kP);
-         * SmartDashboard.putNumber("I Gain", kI);
-         * SmartDashboard.putNumber("D Gain", kD);
-         * SmartDashboard.putNumber("I Zone", kIz);
-         * SmartDashboard.putNumber("Feed Forward", kFF);
-         * SmartDashboard.putNumber("Max Output", kMaxOutput);
-         * SmartDashboard.putNumber("Min Output", kMinOutput);
-         */
+        double smartMotionMaxVel = 30;
+        double smartMotionOutputVel = 0;
+        double smartMotionMaxAccel = 600;
+
+        // TODO pid/smart motion cooefficients for intake
+
+        deploymentPidController.setP(P);
+        deploymentPidController.setI(I);
+        deploymentPidController.setD(D);
+        deploymentPidController.setIZone(IZone);
+        deploymentPidController.setFF(feedForward);
+        deploymentPidController.setOutputRange(minOutput, maxOutput);
+
+        deploymentPidController.setSmartMotionMaxVelocity(smartMotionMaxVel, 0);
+        deploymentPidController.setSmartMotionMinOutputVelocity(smartMotionOutputVel, 0);
+        deploymentPidController.setSmartMotionMaxAccel(smartMotionMaxAccel, 0);
+
+        // the value here is in rotations
+        deploymentPidController.setReference(0.25, CANSparkMax.ControlType.kSmartMotion);
 
         IntakeMap map = new IntakeMap(new PIDSparkMax(3, MotorType.kBrushless),
                 deploymentMotor, outsideLimit::get, insideLimit::get);
