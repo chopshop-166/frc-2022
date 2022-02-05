f859f14d4a8c50d4b7cf064718a6651013749da4
Jeff Burke <jburke0519@gmail.com>
2022-02-04 22:12:54 -0500

    Format

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,10 +8,10 @@ import frc.robot.subsystems.Drive;
 
 public class Robot extends CommandRobot {
 
-  final private ButtonXboxController driveController = new ButtonXboxController(0);
-  final private ButtonXboxController copilotController = new ButtonXboxController(1);
+  private final ButtonXboxController driveController = new ButtonXboxController(0);
+  private final ButtonXboxController copilotController = new ButtonXboxController(1);
 
-  final private RobotMap map = getRobotMap(RobotMap.class, "frc.robot.maps", new RobotMap());
+  private final RobotMap map = getRobotMap(RobotMap.class, "frc.robot.maps", new RobotMap());
 
   private final Drive drive = new Drive(map.getSwerveDriveMap());
 
