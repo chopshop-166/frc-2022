29afad0f148276c19098c4f32231d3ade42d6638
Joe Janicki <joejanicki26@gmail.com>
2022-03-04 20:55:46 -0500

    Add LED animation

--- src/main/java/frc/robot/subsystems/Led.java
+++ src/main/java/frc/robot/subsystems/Led.java
@@ -4,16 +4,33 @@ import com.chopshop166.chopshoplib.commands.SmartSubsystemBase;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.maps.RobotMap.LedMap;
+import frc.robot.util.LightAnimation;
 
 public class Led extends SmartSubsystemBase {
 
     private final AddressableLED led;
     private final AddressableLEDBuffer ledBuffer;
 
+    private int frame = 0;
+    private int timer = 0;
+
     public Led(final LedMap map) {
         led = map.getLed();
         ledBuffer = map.getLedBuffer();
+    }
+
+    public CommandBase animate(LightAnimation animation) {
+        return running("Animate", () -> {
+            if (timer % 3 == 0) {
+                for (int i = 0; i < ledBuffer.getLength(); i++) {
+                    ledBuffer.setLED(i, animation.getColor(frame / (ledBuffer.getLength() / 10), i));
+                }
+                frame++;
+            }
+            timer++;
+        });
     }
 
     @Override
