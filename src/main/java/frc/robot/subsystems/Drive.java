1f1f301946337c5724861332609330ed32957f76
Matt Soucy <git@msoucy.me>
2022-03-04 21:11:57 -0500

    Whitespace cleanup

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -17,96 +17,96 @@ import frc.robot.maps.RobotMap.SwerveDriveMap;
 
 public class Drive extends SmartSubsystemBase {
 
-  private final SwerveDriveKinematics kinematics;
-  private final SwerveModule frontLeft;
-  private final SwerveModule frontRight;
-  private final SwerveModule rearLeft;
-  private final SwerveModule rearRight;
+    private final SwerveDriveKinematics kinematics;
+    private final SwerveModule frontLeft;
+    private final SwerveModule frontRight;
+    private final SwerveModule rearLeft;
+    private final SwerveModule rearRight;
 
-  private final double maxDriveSpeedMetersPerSecond;
-  private final double maxRotationRadiansPerSecond;
-  private final Gyro gyro;
+    private final double maxDriveSpeedMetersPerSecond;
+    private final double maxRotationRadiansPerSecond;
+    private final Gyro gyro;
 
-  public Drive(final SwerveDriveMap map) {
-    super();
+    public Drive(final SwerveDriveMap map) {
+        super();
 
-    frontLeft = map.getFrontLeft();
-    frontRight = map.getFrontRight();
-    rearLeft = map.getRearLeft();
-    rearRight = map.getRearRight();
-    kinematics = new SwerveDriveKinematics(frontLeft.getLocation(), frontRight.getLocation(),
-        rearLeft.getLocation(), rearRight.getLocation());
-    gyro = map.getGyro();
-    maxDriveSpeedMetersPerSecond = map.getMaxDriveSpeedMetersPerSecond();
-    maxRotationRadiansPerSecond = map.getMaxRotationRadianPerSecond();
-  }
+        frontLeft = map.getFrontLeft();
+        frontRight = map.getFrontRight();
+        rearLeft = map.getRearLeft();
+        rearRight = map.getRearRight();
+        kinematics = new SwerveDriveKinematics(frontLeft.getLocation(), frontRight.getLocation(),
+                rearLeft.getLocation(), rearRight.getLocation());
+        gyro = map.getGyro();
+        maxDriveSpeedMetersPerSecond = map.getMaxDriveSpeedMetersPerSecond();
+        maxRotationRadiansPerSecond = map.getMaxRotationRadianPerSecond();
+    }
 
-  public CommandBase fieldCentricDrive(final DoubleSupplier translateX, final DoubleSupplier translateY,
-      final DoubleSupplier rotation) {
-    return running("Field Centric Drive", () -> updateSwerveSpeedAngle(translateX, translateY, rotation));
-  }
+    public CommandBase fieldCentricDrive(final DoubleSupplier translateX, final DoubleSupplier translateY,
+            final DoubleSupplier rotation) {
+        return running("Field Centric Drive", () -> updateSwerveSpeedAngle(translateX, translateY, rotation));
+    }
 
-  private void updateSwerveSpeedAngle(final DoubleSupplier translateX, final DoubleSupplier translateY,
-      final DoubleSupplier rotation) {
-    // Need to convert inputs from -1..1 scale to m/s
-    final Modifier deadband = Modifier.deadband(0.12);
-    final double translateXSpeed = deadband.applyAsDouble(translateX.getAsDouble()) * maxDriveSpeedMetersPerSecond;
-    final double translateYSpeed = deadband.applyAsDouble(translateY.getAsDouble()) * maxDriveSpeedMetersPerSecond;
-    final double rotationSpeed = deadband.applyAsDouble(rotation.getAsDouble()) * maxRotationRadiansPerSecond;
-    SmartDashboard.putNumber("Translate Y", translateYSpeed);
-    SmartDashboard.putNumber("Translate X", translateXSpeed);
-    SmartDashboard.putNumber("Rotation Speed", rotationSpeed);
-    final ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(translateYSpeed, translateXSpeed,
-        rotationSpeed, Rotation2d.fromDegrees(-gyro.getAngle()));
+    private void updateSwerveSpeedAngle(final DoubleSupplier translateX, final DoubleSupplier translateY,
+            final DoubleSupplier rotation) {
+        // Need to convert inputs from -1..1 scale to m/s
+        final Modifier deadband = Modifier.deadband(0.12);
+        final double translateXSpeed = deadband.applyAsDouble(translateX.getAsDouble()) * maxDriveSpeedMetersPerSecond;
+        final double translateYSpeed = deadband.applyAsDouble(translateY.getAsDouble()) * maxDriveSpeedMetersPerSecond;
+        final double rotationSpeed = deadband.applyAsDouble(rotation.getAsDouble()) * maxRotationRadiansPerSecond;
+        SmartDashboard.putNumber("Translate Y", translateYSpeed);
+        SmartDashboard.putNumber("Translate X", translateXSpeed);
+        SmartDashboard.putNumber("Rotation Speed", rotationSpeed);
+        final ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(translateYSpeed, translateXSpeed,
+                rotationSpeed, Rotation2d.fromDegrees(-gyro.getAngle()));
 
-    // Now use this in our kinematics
-    final SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
+        // Now use this in our kinematics
+        final SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
 
-    // Front left module state
-    frontLeft.setDesiredState(moduleStates[0]);
+        // Front left module state
+        frontLeft.setDesiredState(moduleStates[0]);
 
-    // Front right module state
-    frontRight.setDesiredState(moduleStates[1]);
+        // Front right module state
+        frontRight.setDesiredState(moduleStates[1]);
 
-    // Back left module state
-    rearLeft.setDesiredState(moduleStates[2]);
+        // Back left module state
+        rearLeft.setDesiredState(moduleStates[2]);
 
-    // Back right module state
-    rearRight.setDesiredState(moduleStates[3]);
-  }
+        // Back right module state
+        rearRight.setDesiredState(moduleStates[3]);
+    }
 
-  public CommandBase driveDistanceY(final double distance) {
-    return functional("Drive Distance Y", () -> {
-      frontLeft.resetDistance();
-    }, () -> {
-      updateSwerveSpeedAngle(() -> 0, () -> Math.signum(distance) * 0.2, () -> 0);
-    }, (interrupted) -> {
-      updateSwerveSpeedAngle(() -> 0, () -> 0, () -> 0);
-    }, () -> {
-      return Math.abs(frontLeft.getDistance()) >= Math.abs(distance);
-    });
-  }
+    public CommandBase driveDistanceY(final double distance) {
+        return functional("Drive Distance Y", () -> {
+            frontLeft.resetDistance();
+        }, () -> {
+            updateSwerveSpeedAngle(() -> 0, () -> Math.signum(distance) * 0.2, () -> 0);
+        }, (interrupted) -> {
+            updateSwerveSpeedAngle(() -> 0, () -> 0, () -> 0);
+        }, () -> {
+            return Math.abs(frontLeft.getDistance()) >= Math.abs(distance);
+        });
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 
-  @Override
-  public void reset() {
-    gyro.reset();
+    @Override
+    public void reset() {
+        gyro.reset();
 
-    // TODO Default wheels to start position?
+        // TODO Default wheels to start position?
 
-  }
+    }
 
-  @Override
-  public void safeState() {
-    final SwerveModuleState stop = new SwerveModuleState(0.0, new Rotation2d(0, 0));
+    @Override
+    public void safeState() {
+        final SwerveModuleState stop = new SwerveModuleState(0.0, new Rotation2d(0, 0));
 
-    frontLeft.setDesiredState(stop);
-    frontRight.setDesiredState(stop);
-    rearLeft.setDesiredState(stop);
-    rearRight.setDesiredState(stop);
-  }
+        frontLeft.setDesiredState(stop);
+        frontRight.setDesiredState(stop);
+        rearLeft.setDesiredState(stop);
+        rearRight.setDesiredState(stop);
+    }
 }
