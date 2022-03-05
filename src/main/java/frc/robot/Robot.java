96e66859f298f952f0cac7a40290434c1e13a23c
Joe Janicki <joejanicki26@gmail.com>
2022-03-04 21:01:53 -0500

    Rainbow animation as default command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,100 +13,111 @@ import frc.robot.maps.RobotMap;
 import frc.robot.subsystems.BallTransport;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Climber.ExtendDirection;
+import frc.robot.util.LightAnimation;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Led;
 
 public class Robot extends CommandRobot {
 
-  private final ButtonXboxController driveController = new ButtonXboxController(0);
-  private final ButtonXboxController copilotController = new ButtonXboxController(1);
+    private final ButtonXboxController driveController = new ButtonXboxController(0);
+    private final ButtonXboxController copilotController = new ButtonXboxController(1);
 
-  private final RobotMap map = getRobotMap(RobotMap.class, "frc.robot.maps", new RobotMap());
+    private final RobotMap map = getRobotMap(RobotMap.class, "frc.robot.maps", new RobotMap());
 
-  private final Drive drive = new Drive(map.getSwerveDriveMap());
+    private final Drive drive = new Drive(map.getSwerveDriveMap());
 
-  private final Intake intake = new Intake(map.getIntakeMap());
+    private final Intake intake = new Intake(map.getIntakeMap());
 
-  private final BallTransport ballTransport = new BallTransport(map.getBallTransportMap());
+    private final Led led = new Led(map.getLedMap());
 
-  private final Climber leftClimber = new Climber(map.getLeftClimberMap());
-  private final Climber rightClimber = new Climber(map.getRightClimberMap());
+    private final BallTransport ballTransport = new BallTransport(map.getBallTransportMap());
 
-  @Override
-  public void robotInit() {
-    super.robotInit();
-  }
+    private final Climber leftClimber = new Climber(map.getLeftClimberMap());
+    private final Climber rightClimber = new Climber(map.getRightClimberMap());
 
-  @Override
-  public void configureButtonBindings() {
-    DoubleSupplier climberTrigger = copilotController::getTriggers;
-    DoubleSupplier climberJoystickX = copilotController::getLeftX;
+    private final LightAnimation rainbowAnimation = new LightAnimation("rainbow.json");
 
-    driveController.back().whenPressed(drive.resetCmd());
+    @Override
+    public void robotInit() {
+        super.robotInit();
+    }
 
-    // Intake:
-    // On button press: extend intake and start roller
-    // On button release: retract intake and stop roller
+    @Override
+    public void configureButtonBindings() {
+        DoubleSupplier climberTrigger = copilotController::getTriggers;
+        DoubleSupplier climberJoystickX = copilotController::getLeftX;
 
-    // Soon to be shooter command
-    driveController.x().whenPressed(ballTransport.loadShooter());
+        driveController.back().whenPressed(drive.resetCmd());
 
-    driveController.a().whenPressed(intake.extend())
-        .whileHeld(
-            sequence("Start Intake and Transporter", intake.startRoller(SpinDirection.COUNTERCLOCKWISE),
-                ballTransport.loadCargoWithIntake()))
-        .whenReleased(sequence("Ball transport end", race("Finish Transport", new WaitCommand(2), ballTransport
-            .loadCargoWithIntake()),
-            parallel("Intake retracted w/ Ball Transport", ballTransport.stopTransport(), intake.retract())));
-    driveController.y()
-        .whenPressed(
-            sequence("Remove Wrong Colored Balls", intake.extend(), intake.startRoller(SpinDirection.CLOCKWISE),
-                ballTransport.removeCargo(), intake.retract()));
+        // Intake:
+        // On button press: extend intake and start roller
+        // On button release: retract intake and stop roller
 
-    SmartDashboard.putData("Run Top Backwards", ballTransport.runTopBackwards());
-    SmartDashboard.putData("Run Bottom Backwards", ballTransport.runBottomBackwards());
-    SmartDashboard.putData("Only Roll Intake Forwards", intake.startRoller(SpinDirection.COUNTERCLOCKWISE));
-    SmartDashboard.putData("Stop Intake", intake.stopRoller());
+        // Soon to be shooter command
+        driveController.x().whenPressed(ballTransport.loadShooter());
 
-    // Climber:
-    copilotController.x()
-        .whileHeld(parallel("Extend Triggers", leftClimber.extendSpeed(
-            climberTrigger), rightClimber.extendSpeed(climberTrigger)));
-    copilotController.y()
-        .whileHeld(parallel("Rotate", leftClimber.rotateSpeed(
-            climberJoystickX),
-            rightClimber.rotateSpeed(
-                climberJoystickX)));
+        driveController.a().whenPressed(intake.extend())
+                .whileHeld(
+                        sequence("Start Intake and Transporter", intake.startRoller(SpinDirection.COUNTERCLOCKWISE),
+                                ballTransport.loadCargoWithIntake()))
+                .whenReleased(sequence("Ball transport end", race("Finish Transport", new WaitCommand(2), ballTransport
+                        .loadCargoWithIntake()),
+                        parallel("Intake retracted w/ Ball Transport", ballTransport.stopTransport(),
+                                intake.retract())));
+        driveController.y()
+                .whenPressed(
+                        sequence("Remove Wrong Colored Balls", intake.extend(),
+                                intake.startRoller(SpinDirection.CLOCKWISE),
+                                ballTransport.removeCargo(), intake.retract()));
 
-    copilotController.getPovButton(POVDirection.LEFT)
-        .whileHeld(parallel("Rotate CCW", leftClimber.rotate(SpinDirection.COUNTERCLOCKWISE),
-            rightClimber.rotate(SpinDirection.COUNTERCLOCKWISE)));
-    copilotController.getPovButton(POVDirection.RIGHT)
-        .whileHeld(parallel("Rotate CW", leftClimber.rotate(SpinDirection.CLOCKWISE),
-            rightClimber.rotate(SpinDirection.CLOCKWISE)));
-    copilotController.a()
-        .whileHeld(parallel("Extend", leftClimber.extend(ExtendDirection.EXTEND),
-            rightClimber.extend(ExtendDirection.EXTEND)));
-    copilotController.b()
-        .whileHeld(parallel("Retract", leftClimber.extend(ExtendDirection.RETRACT),
-            rightClimber.extend(ExtendDirection.RETRACT)));
+        SmartDashboard.putData("Run Top Backwards", ballTransport.runTopBackwards());
+        SmartDashboard.putData("Run Bottom Backwards", ballTransport.runBottomBackwards());
+        SmartDashboard.putData("Only Roll Intake Forwards", intake.startRoller(SpinDirection.COUNTERCLOCKWISE));
+        SmartDashboard.putData("Stop Intake", intake.stopRoller());
 
-    // Stop all subsystems
-    copilotController.back().whenPressed(cmd("Stop All").onInitialize(() -> {
-      safeStateAll();
-    }));
-  }
+        // Climber:
+        copilotController.x()
+                .whileHeld(parallel("Extend Triggers", leftClimber.extendSpeed(
+                        climberTrigger), rightClimber.extendSpeed(climberTrigger)));
+        copilotController.y()
+                .whileHeld(parallel("Rotate", leftClimber.rotateSpeed(
+                        climberJoystickX),
+                        rightClimber.rotateSpeed(
+                                climberJoystickX)));
 
-  @Override
-  public void populateDashboard() {
+        copilotController.getPovButton(POVDirection.LEFT)
+                .whileHeld(parallel("Rotate CCW", leftClimber.rotate(SpinDirection.COUNTERCLOCKWISE),
+                        rightClimber.rotate(SpinDirection.COUNTERCLOCKWISE)));
+        copilotController.getPovButton(POVDirection.RIGHT)
+                .whileHeld(parallel("Rotate CW", leftClimber.rotate(SpinDirection.CLOCKWISE),
+                        rightClimber.rotate(SpinDirection.CLOCKWISE)));
+        copilotController.a()
+                .whileHeld(parallel("Extend", leftClimber.extend(ExtendDirection.EXTEND),
+                        rightClimber.extend(ExtendDirection.EXTEND)));
+        copilotController.b()
+                .whileHeld(parallel("Retract", leftClimber.extend(ExtendDirection.RETRACT),
+                        rightClimber.extend(ExtendDirection.RETRACT)));
 
-    // TODO Auto-generated method stub
-  }
+        // Stop all subsystems
+        copilotController.back().whenPressed(cmd("Stop All").onInitialize(() -> {
+            safeStateAll();
+        }));
+    }
 
-  @Override
-  public void setDefaultCommands() {
-    drive.setDefaultCommand(
-        drive.fieldCentricDrive(driveController::getLeftX, driveController::getLeftY, driveController::getRightX));
-    ballTransport.setDefaultCommand(ballTransport.defaultToLaser());
-  }
+    @Override
+    public void populateDashboard() {
+
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void setDefaultCommands() {
+        drive.setDefaultCommand(
+                drive.fieldCentricDrive(driveController::getLeftX, driveController::getLeftY,
+                        driveController::getRightX));
+        ballTransport.setDefaultCommand(ballTransport.defaultToLaser());
+
+        led.setDefaultCommand(led.animate(rainbowAnimation));
+    }
 }
